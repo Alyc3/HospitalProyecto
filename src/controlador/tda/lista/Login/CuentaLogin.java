fcb75ce0d96f4b81d19a10a9d66e467c6fb94f6b
Johnny Armijos <117655231+JohnyAlexander@users.noreply.github.com>
2022-12-28 11:20:13 -0500

    Add files via upload

---
+++ src/controlador/tda/lista/Login/CuentaLogin.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlador.tda.lista.Login;
+
+import controlador.tda.lista.exception.PosicionException;
+import modelo.Cuenta;
+import vista.InicioLogin;
+
+/**
+ *
+ * @author johnny
+ */
+public class CuentaLogin {
+
+    private static InicioLogin logUsuario = new InicioLogin();
+
+    public static boolean validar(String usuario, String clave) {
+        if (obtener(usuario) != null) {
+            Cuenta UCuenta = obtener(usuario);
+            if (UCuenta.getUsuario().equals(usuario) && UCuenta.getClave().equals(clave)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean insertar(Cuenta cuenta) {
+        return logUsuario.ingresar(cuenta);
+    }
+
+    public static boolean modificar(Cuenta cuenta) {
+        return logUsuario.modificar(cuenta);
+    }
+
+    public static boolean eliminar(String cuentas) throws PosicionException {
+        return logUsuario.eliminar(cuentas);
+    }
+
+    public static Cuenta obtener(String cuentas) {
+        return logUsuario.obtener(cuentas);
+    }
+
+}
---
+++ src/vista/InicioLogin.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vista;
+
+import controlador.tda.lista.ListaEnlazada;
+import controlador.tda.lista.exception.PosicionException;
+import modelo.Cuenta;
+
+/**
+ *
+ * @author johnny
+ */
+public class InicioLogin {
+
+    private ListaEnlazada<Cuenta> listCuenta;
+
+    public InicioLogin() {
+        listCuenta = new ListaEnlazada<Cuenta>();
+    }
+
+    public int Buscar(String listUsuario) {
+        int n = -1;
+        for (int i = 0; i < listCuenta.getSize(); i++) {
+            if (listUsuario.get(i).getCuenta().equals(listUsuario)) {
+                n = i;
+                break;
+            }
+        }
+        return n;
+    }
+
+    public boolean ingresar(Cuenta listUsuario) {
+        if (Buscar(listUsuario.getUsuario()) == -1) {
+            listCuenta.insertarCabecera(listUsuario);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean modificar(Cuenta listUsuario) {
+        if (Buscar(listUsuario.getUsuario()) != -1) {
+            Cuenta usuariList = obtener(listUsuario.getUsuario());
+            usuariList.setUsuario(listUsuario.getUsuario());
+            usuariList.setClave(listUsuario.getClave());
+            usuariList.setPersona(listUsuario.getPersona());
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean eliminar(String listUsuario) throws PosicionException {
+        if (Buscar(listUsuario) != -1) {
+            listCuenta.eliminarDato(Buscar(listUsuario));
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public Cuenta obtener(String listUsuario) {
+        if (Buscar(listUsuario) != -1) {
+            return listCuenta.get(Buscar(listUsuario));
+            //return listCuenta.obtenerDato(Buscar(listUsuario));
+        } else {
+            return null;
+        }
+    }
+}
