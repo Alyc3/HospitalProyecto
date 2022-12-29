13ef50697f21e692a9b4c6625f9b7f6ae30eb837
IvanG0nzalez <104579593+IvanG0nzalez@users.noreply.github.com>
2022-12-28 21:53:09 -0500

    Add files via upload

---
+++ HospitalProyecto/src/modelo/Examen.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+import modelo.enums.TipoExamen;
+
+/**
+ *
+ * @author Iván González
+ */
+public class Examen {
+
+    private TipoExamen tipoExamen;
+    private String tiempoEntrega;
+    private ConsultaMedica consultaMedica;
+
+    public TipoExamen getTipoExamen() {
+        return tipoExamen;
+    }
+
+    public void setTipoExamen(TipoExamen tipoExamen) {
+        this.tipoExamen = tipoExamen;
+    }
+
+    public String getTiempoEntrega() {
+        return tiempoEntrega;
+    }
+
+    public void setTiempoEntrega(String tiempoEntrega) {
+        this.tiempoEntrega = tiempoEntrega;
+    }
+
+    public ConsultaMedica getConsultaMedica() {
+        return consultaMedica;
+    }
+
+    public void setConsultaMedica(ConsultaMedica consultaMedica) {
+        this.consultaMedica = consultaMedica;
+    }
+
+}
