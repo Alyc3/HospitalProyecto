68d7deac84bd5bc1efddd9d6b6644ee08b1a5ec2
Johnny Armijos <117655231+JohnyAlexander@users.noreply.github.com>
2023-02-19 18:50:02 -0500

    Servicios y Diagnostico
    Métodos para la codificación y decodificación de Diagnostico, e implementación de servicios login y register de persona

---
+++ ProyectoHospital/src/hospital/repositorio/RepositorioHistorialMedico.java
@@ -0,0 +1,40 @@
+package hospital.repositorio;
+
+import hospital.modelo.HistorialMedico;
+import hospital.modelo.excepciones.ModeloException;
+
+public interface RepositorioHistorialMedico {
+
+    /**
+     * Método para crear un Historial Médico
+     * @param historialMedico
+     * @return
+     * @throws ModeloException 
+     */
+    public HistorialMedico crear(HistorialMedico historialMedico) throws ModeloException;
+
+    /**
+     * Método para actualizar un Historial Médico
+     * @param historialMedico
+     * @return
+     * @throws ModeloException 
+     */
+    public HistorialMedico actualizar(HistorialMedico historialMedico) throws ModeloException;
+
+    /**
+     * Método para encontrar un Historial Médico por medio de su id
+     * @param id
+     * @return
+     * @throws ModeloException 
+     */
+    public HistorialMedico encontrarPorId(Integer id) throws ModeloException;
+
+    /**
+     * Método para encontrar un Historial Médico por medio del id de la persona
+     * a la que se encuentra asociado
+     * @param idPersona
+     * @return
+     * @throws ModeloException 
+     */
+    public HistorialMedico encontrarPorIdPersona(Integer idPersona) throws ModeloException;
+}
